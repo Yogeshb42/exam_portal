a5380f523594cd2396ff7c642f547838fd655726
Yogeshb42 <yogeshbhagat4242@gmail.com>
2023-09-14 12:12:46 +0530

    Update settings.py

--- ExamPortal/settings.py
+++ ExamPortal/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-&!8fhc$^q)$e(cf65(@20&znbc222*^srusy+9z)lqag_*7$%b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['exam-portal-yajl.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
